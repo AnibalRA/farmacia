f27733cdd7089b4afafe6811082ed597c1ab43ac
Anibal <ingenieroanibal@gmail.com>
2014-11-26 17:49:48 -0600

    Navbar
    Menu principal

--- app/views/index/librerias_css.blade.php
+++ app/views/index/librerias_css.blade.php
@@ -1,1 +1,2 @@
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+{{ HTML::style('assets/css/estilos.css', array('media' => 'screen')) }}
--- app/views/index/menu.blade.php
+++ app/views/index/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -8,13 +8,16 @@
         </button>
         <a class="navbar-brand" href="#">Logo</a>
     </div>
-    <div class="collapse navbar-collapse navbar-inverse-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li>{{ HTML::link('/', 'inicio'); }}</li>
-        </ul>
+    <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav navbar-right">
+            <li><a href="javascript:void(0)"><span class="glyphicon glyphicon-home"></span> Nombre Farmacia</a></li>
+            <li><a href="javascript:void(0)">-</a></li>
+            <li><a href="javascript:void(0)"><span class="glyphicon glyphicon-user"></span> Nombre Usuario</a></li>
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Configuración<strong class="caret"></strong></a>
+                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="glyphicon glyphicon-cog"> </span>
+                    <strong class="caret"></strong>
+                </a>
                 <ul class="dropdown-menu">
                     <li>{{ HTML::link('usuario', 'Usuarios'); }}</li>
                     <li>{{ HTML::link('tipo_usuario', 'Tipos de Usuarios'); }}</li>
